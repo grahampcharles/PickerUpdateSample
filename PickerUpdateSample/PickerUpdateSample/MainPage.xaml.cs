a6c285220538598d34c6809f77d68b58f9b987a7
Graham Charles <grahampcharles@gmail.com>
2018-10-14 13:31:39 -0700

    initial commit

---
+++ PickerUpdateSample/PickerUpdateSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickerUpdateSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private ViewModel vm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            vm = new ViewModel();
+            this.BindingContext = vm;
+
+            // show the first item, just to make the issue clearer
+            thePicker.SelectedIndex = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            vm.MyItems[0].MyItemName = "New Random Name:" + RandomString(6);
+            ResultsLabel.IsVisible = true;
+
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ PickerUpdateSample/PickerUpdateSample/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PickerUpdateSample
+{
+    class ViewModel : BaseViewModel 
+    {
+        public ObservableCollection<MyItem> MyItems { get; set; }
+
+        public ViewModel()
+        {
+            MyItems = new ObservableCollection<MyItem>();
+            MyItems.Add(new MyItem() { MyItemName = "Item 1" });
+            MyItems.Add(new MyItem() { MyItemName = "Item 2" });
+            MyItems.Add(new MyItem() { MyItemName = "Item 3" });
+        }
+
+        public string FirstItemName
+        {
+            get { return MyItems[0].MyItemName; }
+        }
+    }
+
+    class MyItem : ObservableObject
+    {
+        private string myItemName;
+        public string MyItemName { get => myItemName; set => SetProperty(ref myItemName, value); }
+    }
+}
